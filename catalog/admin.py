833b09b0a3f96b4d90d18c164a214b6ad4a12ea9
oluwaseunolusanya <oluwaseun_olusanya@yahoo.com>
2023-08-15 09:23:06 +0100

    Defined and registered admin classes for models in 'admin.py' and defined a helper function for displaying genre in 'model.py'.

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,9 +1,24 @@
 from django.contrib import admin
 from .models import Author, Genre, Book, BookInstance, Language
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
 
-admin.site.register(Book)
-admin.site.register(Author)
+#admin.site.register(Book, BookAdmin)
+
+# Define admin class
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+# Register class with associated model
+#admin.site.register(Author, AuthorAdmin)
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
+
+#admin.site.register(BookInstance,BookInstanceAdmin)
+
 admin.site.register(Genre)
-admin.site.register(BookInstance)
 admin.site.register(Language)
 
