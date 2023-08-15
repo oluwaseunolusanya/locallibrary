cc480f815b74c1f193d855eba1dcec893a71ee53
oluwaseunolusanya <oluwaseun_olusanya@yahoo.com>
2023-08-15 09:41:10 +0100

    Organised the layout of the detail views for the records

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from .models import Author, Genre, Book, BookInstance, Language
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
 
 #admin.site.register(Book, BookAdmin)
 
@@ -10,12 +15,21 @@ class BookAdmin(admin.ModelAdmin):
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
 
 # Register class with associated model
 #admin.site.register(Author, AuthorAdmin)
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ('status','due_back')
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
 
 #admin.site.register(BookInstance,BookInstanceAdmin)
 
